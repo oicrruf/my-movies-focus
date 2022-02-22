3a3c968e90e679c9a5e3bff6c117b602035a9b34
Víctor M. Reyes <vm.reyesal@gmail.com>
2022-02-22 00:50:27 -0600

    feat: add .editorconfig file

--- App.tsx
+++ App.tsx
@@ -7,54 +7,54 @@ import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
 const Splash: React.FC = () => {
-  return (
-    <View style={styles.box}>
-      <LottieView
-        style={styles.animation}
-        source={require('./assets/movie-cut.json')}
-        autoPlay
-        loop
-      />
-    </View>
-  );
+	return (
+		<View style={styles.box}>
+			<LottieView
+				style={styles.animation}
+				source={require('./assets/movie-cut.json')}
+				autoPlay
+				loop
+			/>
+		</View>
+	);
 };
 
 const Login: React.FC = () => {
-  return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  );
+	return (
+		<View>
+			<Text>Login</Text>
+		</View>
+	);
 };
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	}, []);
 
-  if (loading) {
-    return <Splash />;
-  } else {
-    return <Login />;
-  }
+	if (loading) {
+		return <Splash />;
+	} else {
+		return <Login />;
+	}
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  box: {
-    flex: 1,
-    width: '100%',
-    justifyContent: 'center',
-  },
-  animation: {
-    width: '40%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignSelf: 'center',
-  },
+	box: {
+		flex: 1,
+		width: '100%',
+		justifyContent: 'center',
+	},
+	animation: {
+		width: '40%',
+		display: 'flex',
+		flexDirection: 'column',
+		alignSelf: 'center',
+	},
 });
