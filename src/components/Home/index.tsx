0ff082dd47f3b7ea6af911756a7017276026439b
Víctor M. Reyes <vm.reyesal@gmail.com>
2022-02-23 13:14:03 -0600

    feat: add bottom sheet for detail

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,17 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import {
 	StyleSheet,
 	Text,
 	View,
 	FlatList,
 	ActivityIndicator,
+	TouchableWithoutFeedback,
 } from 'react-native';
 import {Image} from 'react-native-elements/dist/image/Image';
 import {getPopularMovies} from '../../utils';
 import LinearGradient from 'react-native-linear-gradient';
+import RBSheet from 'react-native-raw-bottom-sheet';
 
 const Home: React.FC = () => {
 	const [popularMovies, setPopularMovies] = useState([]);
+	const moviesRef = useRef([]);
 
 	useEffect(() => {
 		getPopularMovies()
@@ -43,15 +46,51 @@ const Home: React.FC = () => {
 				</View>
 			</View>
 		) : (
-			<View style={styles.others}>
-				<Image
-					source={{
-						uri: `https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`,
-					}}
-					containerStyle={styles.image}
-					PlaceholderContent={<ActivityIndicator />}
-				/>
-			</View>
+			<TouchableWithoutFeedback onPress={() => moviesRef.current[index].open()}>
+				<View style={styles.others}>
+					<Image
+						source={{
+							uri: `https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`,
+						}}
+						containerStyle={styles.image}
+						PlaceholderContent={<ActivityIndicator />}
+					/>
+					<RBSheet
+						ref={el => (moviesRef.current[index] = el)}
+						closeOnDragDown={true}
+						closeOnPressMask={true}
+						customStyles={{
+							container: {
+								backgroundColor: '#2b2b2b',
+								borderTopLeftRadius: 10,
+								borderTopRightRadius: 10,
+								padding: 10,
+							},
+							wrapper: {
+								backgroundColor: 'rgba(0,0,0,0.2)',
+							},
+							draggableIcon: {
+								backgroundColor: '#000',
+							},
+						}}>
+						<View style={styles.info}>
+							<Image
+								source={{
+									uri: `https://www.themoviedb.org/t/p/w220_and_h330_face/${item.poster_path}`,
+								}}
+								containerStyle={styles.imageSheet}
+								PlaceholderContent={<ActivityIndicator />}
+							/>
+							<View style={styles.infoDetail}>
+								<Text style={styles.text}>{item.original_title}</Text>
+								<Text style={styles.text}>{item.release_date}</Text>
+								<Text style={styles.text}>{item.overview}</Text>
+								<Text style={styles.text}>{item.vote_average}</Text>
+							</View>
+						</View>
+					</RBSheet>
+				</View>
+			</TouchableWithoutFeedback>
 		);
 
 	return (
@@ -73,10 +112,23 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: '#000',
 	},
+	info: {
+		flexDirection: 'row',
+		backgroundColor: '#2b2b2b',
+	},
+	infoDetail: {
+		flexDirection: 'column',
+		backgroundColor: '#2b2b2b',
+		marginHorizontal: 10,
+	},
 	image: {
 		aspectRatio: 9 / 16,
 		width: '100%',
 		marginVertical: 10,
+	},
+	imageSheet: {
+		aspectRatio: 9 / 16,
+		width: '25%',
 	},
 	mainContainer: {
 		flexDirection: 'row',
